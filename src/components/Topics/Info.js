7c945877fc42c433f1fd0c34a52ae05f3562c9a9
Logan Simonsen <32521559+ENDgameERS@users.noreply.github.com>
2018-01-28 21:15:29 -0600

    minor update to info.js

--- src/components/Topics/Info.js
+++ src/components/Topics/Info.js
@@ -5,10 +5,10 @@ export default class Info extends Component {
   render() {
     return (
       <div className="puzzleBox sumPB">
-        <h4> Logan Simonsen </h4>
-        React App Week 1 Project<br />
-        <br />DevMountain Cohort 11<br />
+      <h5> Logan Simonsen <br />React App Week 1 Project<br />
+      DevMountain Dallas Cohort 11<br /></h5>
         <img id='DMimage' src="https://s3.amazonaws.com/devmountain/www/img/dm_white_logo.png" />
+        
       </div>
     )
   }
